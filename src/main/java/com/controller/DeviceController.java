f8a75a133868d5566d3e567e158db8cc01a326e5
Shao Zengwei <zengwei19861029@163.com>
2016-03-20 16:24:54 +0800

    add log4j into our infrustructure. zengwei shao

--- src/main/java/com/controller/DeviceController.java
+++ src/main/java/com/controller/DeviceController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import com.bean.DeviceModel;
 import com.service.DeviceService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,12 +13,16 @@ import java.util.List;
 @Controller
 public class DeviceController {
 
+    Logger logger = Logger.getLogger(DeviceController.class);
     @Autowired
     DeviceService deviceService;
 
     @RequestMapping(value = "/deviceInfo")
     @ResponseBody
     public List<DeviceModel> getDevicesInfo() {
+
+        logger.error("this is error");
+        logger.info("this is info");
         return deviceService.getDevicesInfo();
     }
 }
