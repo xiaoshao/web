828e936555044ea81c7344d922cf699ca0b94a2d
Shao Zengwei <zengwei19861029@163.com>
2016-03-20 18:05:23 +0800

    redirect to login page  when did not login.

--- src/main/java/com/interceptor/CookieCheckInterceptor.java
+++ src/main/java/com/interceptor/CookieCheckInterceptor.java
@@ -2,9 +2,12 @@ package com.interceptor;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class CookieCheckInterceptor extends HandlerInterceptorAdapter {
 
@@ -16,16 +19,23 @@ public class CookieCheckInterceptor extends HandlerInterceptorAdapter {
 
         Cookie[] cookies = request.getCookies();
 
-        if (!validCookie(cookies)) {
-            throw new RuntimeException("Did not login.");
+        if (cookies == null || !validCookie(cookies)) {
+            redirectToLogin(request, response);
+            return false;
         }
 
         return super.preHandle(request, response, handler);
     }
 
+    private void redirectToLogin(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_PAYMENT_REQUIRED);
+        RequestDispatcher rd = request.getRequestDispatcher("index.html");
+        rd.forward(request, response);
+    }
+
     private boolean validCookie(Cookie[] cookies) {
         for (Cookie cookie : cookies) {
-            if ("1234567".equals(cookies[0].getValue())) {
+            if ("1234567".equals(cookie.getValue())) {
                 return true;
             }
         }
