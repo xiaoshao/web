19583cb506087093aa3d3e39baf57b3a28c485cb
Shao Zengwei <zengwei19861029@163.com>
2016-03-20 12:10:59 +0800

    add blacklist basic function. zengwei shao

--- src/main/java/com/controller/BlackListController.java
+++ src/main/java/com/controller/BlackListController.java
@@ -5,6 +5,7 @@ import com.service.BlacklistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class BlacklistController {
     BlacklistService blacklistService;
 
     @RequestMapping(value = "/blacklist")
+    @ResponseBody
     public List<BlacklistModel> getBlacklist() {
         return blacklistService.getBlacklist();
     }
