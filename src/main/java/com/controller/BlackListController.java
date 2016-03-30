33f4c46fe261de759d73ba8996d323f5a88f6522
Shao Zengwei <zengwei19861029@163.com>
2016-03-30 17:42:02 +0800

    web sso beginning. zengwei shao

--- src/main/java/com/controller/BlackListController.java
+++ src/main/java/com/controller/BlackListController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
-public class BlacklistController {
+public class BlackListController {
 
     @Autowired
     BlacklistService blacklistService;
